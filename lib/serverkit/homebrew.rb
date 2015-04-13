fa0641b2c25a3f48970527ada1e692ecf9575997
Ryo Nakamura <r7kamura@gmail.com>
2015-04-13 19:18:16 +0900

    bundle gem serverkit-homebrew

---
+++ lib/serverkit/homebrew.rb
@@ -0,0 +1,7 @@
+require "serverkit/homebrew/version"
+
+module Serverkit
+  module Homebrew
+    # Your code goes here...
+  end
+end
---
+++ lib/serverkit/homebrew/version.rb
@@ -0,0 +1,5 @@
+module Serverkit
+  module Homebrew
+    VERSION = "0.1.0"
+  end
+end
