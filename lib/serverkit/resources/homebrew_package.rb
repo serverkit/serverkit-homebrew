4854633196c31a53415cbfb3dd8aeeac473ad78b
Ryo Nakamura <r7kamura@gmail.com>
2015-04-13 20:18:25 +0900

    Support formula on other GitHub repos
    (e.g. caskroom/cask/brew-cask)

--- lib/serverkit/resources/homebrew_package.rb
+++ lib/serverkit/resources/homebrew_package.rb
@@ -3,6 +3,18 @@ require "serverkit/resources/package"
 module Serverkit
   module Resources
     class HomebrewPackage < Package
+      # @note Override to use unnested name
+      def check
+        check_command_from_identifier(:check_package_is_installed, unnested_name, version)
+      end
+
+      private
+
+      # @return [String]
+      # @example "brew-cask" # for "caskroom/cask/brew-cask"
+      def unnested_name
+        name.split("/").last
+      end
     end
   end
 end
